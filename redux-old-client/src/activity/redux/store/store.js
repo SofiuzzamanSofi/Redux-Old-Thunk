4bee2d6a96a58cf93031cbab9510127e449f7162
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2023-03-21 04:53:47 +0600

    added redux store again -2

--- redux-old-client/src/activity/redux/store/store.js
+++ redux-old-client/src/activity/redux/store/store.js
@@ -1,14 +1,14 @@
-
-import { composeWithDevTools } from "@redux-devtools/extension";
 import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
-import rootReducer from "../reducers/rootReducer";
+import thunk from "redux-thunk"
+import rootReducer from "../reducers/rootReducer"
+import { composeWithDevTools } from "@redux-devtools/extension"
+
+
 
 const store = createStore(
     rootReducer,
     composeWithDevTools(applyMiddleware(thunk, logger)),
-
 );
 
 export default store;
