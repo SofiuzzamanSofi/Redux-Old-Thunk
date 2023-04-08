e377fc608efbc90808c4cf6d633be05689d3b142
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-04-08 06:31:54 +0600

    before short function

--- redux-old-client/src/activity/component/Contents/Contents.js
+++ redux-old-client/src/activity/component/Contents/Contents.js
@@ -6,8 +6,10 @@ function Contents() {
     const contents = useSelector(state => state.contents.contents)
 
     return (
-        <div className='grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 pb-10'>
-            {contents?.length && contents.map((content, i) => < ContentCard key={i} content={content} />)}
+        <div>
+            <div className='grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 pb-10'>
+                {contents?.length && contents.map((content, i) => < ContentCard key={i} content={content} />)}
+            </div>
         </div>
     )
 }
--- redux-old-client/src/activity/component/Home/Home.js
+++ redux-old-client/src/activity/component/Home/Home.js
@@ -12,6 +12,19 @@ function Home() {
     return (
         <div>
             <div className='flex justify-center gap-2'>
+                <div>
+
+                    <div className='rounded-md bg-slate-900'>
+                        <select id="sort-by" className='bg-[#17191b] px-4 py-[11px] rounded-md pr-4 border-0'>
+                            <option value="last-update">Short By:</option>
+                            <option value="last-update">Latest</option>
+                            <option value="first-update">Oldest</option>
+                        </select>
+                    </div>
+
+                </div>
+
+
                 <Link to="/dashboard"><button className='btn'>Admin</button></Link>
                 {/* <Link><button className='btn'>User</button></Link> */}
                 {
